01a24924bd3c0e96a54e73622fcc1fe1251c8a51
timhoangt <timothy.hoang1@marist.edu>
2017-05-03 19:01:47 -0400

    tabs to spaces

--- SD1/Labs/7/BMI.java
+++ SD1/Labs/7/BMI.java
@@ -49,17 +49,17 @@ public class BMI {
       ((height * METERS_PER_INCH) * (height *  METERS_PER_INCH));
     return Math.round(bmi * 100) / 100.0;
   }
-
+  
   public String getStatus() {
     double bmi = getBMI();
     if (bmi < 18.5)
-      return "Underweight";
+    return "Underweight";
     else if (bmi < 25)
-      return "Normal";
+    return "Normal";
     else if (bmi < 30)
-      return "Overweight";
+    return "Overweight";
     else
-      return "Obese";
+    return "Obese";
   }
 
   public String getName() {
