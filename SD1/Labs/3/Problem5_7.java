ac0d11641187a05596517073c20bbb95abba06b3
timhoangt <timothy.hoang1@marist.edu>
2017-05-03 18:55:41 -0400

    fixed indentation and compile error

--- SD1/Labs/3/Problem5_7.java
+++ SD1/Labs/3/Problem5_7.java
@@ -20,26 +20,26 @@
 
 public class Problem5_7{
   public static void main(String[] args) {
-  //Declares the variables.
-  double tuition = 10000;
-  double cost = 0;
-  int i;
-  int o;
+    //Declares the variables.
+    double tuition = 10000;
+    double cost = 0;
+    int i;
+    int o;
   
-  //Finds and prints the tuition in ten years.
-  for (i = 0; i < 11; i++){
-    tuition = tuition * 1.05;
-  }
+    //Finds and prints the tuition in ten years.
+    for (i = 0; i < 11; i++){
+      tuition = tuition * 1.05;
+    }
   
-  System.out.println("The tuition in 10 years is " + (Math.round(tuition)));
+    System.out.println("The tuition in 10 years is " + (Math.round(tuition)));
   
-  //Find the tuition for four years after the ten years and adds them together.
-  for (o = 0; o < 5; o++){
-    tuition = tuition * 1.05;
-    cost = tuition + cost;
-  }
+    //Find the tuition for four years after the ten years and adds them together.
+    for (o = 0; o < 5; o++){
+      tuition = tuition * 1.05;
+      cost = tuition + cost;
+    }
   
-  //Prints out the cost of four years' tuition after ten years.
-  System.out.println("The cost of four years' worth of tuition after the tenth year is " + (Math.round(cost)));
+    //Prints out the cost of four years' tuition after ten years.
+    System.out.println("The cost of four years' worth of tuition after the tenth year is " + (Math.round(cost)));
   }
 }
--- SD1/Labs/3/Problem6_3.java
+++ SD1/Labs/3/Problem6_3.java
@@ -21,44 +21,45 @@ import java.util.Scanner;
 
 public class Problem6_3{
   public static void main(String[] args) {
-  Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
   
-  //Prompts a number.
-  System.out.print("Enter an integer: ");
+    //Prompts a number.
+    System.out.print("Enter an integer: ");
     int number = input.nextInt();
   
-  //Prints answer.
-  System.out.println("The reversal of your number is");
-  System.out.println((reverse(number)));
-  System.out.println("True if your number is a palindome, False if not");
-  System.out.println(isPalindome(number));  
+    //Prints answer.
+    System.out.println("The reversal of your number is");
+    System.out.println((reverse(number)));
+    System.out.println("True if your number is a palindome, False if not");
+    System.out.println(isPalindome(number));  
   }
   
   //Takes the number and returns the reverse.
   public static int reverse(int number) {
-  int reversed = 0;
+    int reversed = 0;
   
-  //Loops until all the digits are read.
-  while (number > 0){
-    int digit = number % 10;
-    number = number / 10;
-    reversed = reversed + digit;
-    reversed = reversed * 10;
-  }
-  reversed = reversed / 10;
-
-  return reversed;
+    //Loops until all the digits are read.
+    while (number > 0){
+      int digit = number % 10;
+      number = number / 10;
+      reversed = reversed + digit;
+      reversed = reversed * 10;
+    }
+  
+    reversed = reversed / 10;
+    return reversed;
   }
   
   //Takes the number and returns a T/F if the number is a palindome.
   public static boolean isPalindome(int number){
     boolean isPalindome = false;  
-  if (number == (reverse(number))){
-    isPalindome = true;
-  }
-  else{
-    isPalindome = false;
-  }
-  return isPalindome;
+    
+    if (number == (reverse(number))){
+      isPalindome = true;
+    }
+    else{
+      isPalindome = false;
+    }
+    return isPalindome;
   }
 }
