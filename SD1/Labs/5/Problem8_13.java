e3e500297f0abf79d289a7bd21c8bd7f6fd4e741
timhoangt <timothy.hoang1@marist.edu>
2017-05-03 21:09:38 -0400

    added Problem8_13

--- SD1/Labs/5/Problem8_13.java
+++ SD1/Labs/5/Problem8_13.java
@@ -13,37 +13,56 @@
 /**
  * Problem8_13
  * 
- * This class computes the sum of the digits in an integer.
+ * This class locates the largest element in a two-dimensional aray.
  */
- 
-//Imports the Scanner.
+
 import java.util.Scanner;
 
-public class Problem8_13{
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-  
-    //Prompts a number.
-    System.out.print("Enter an integer: ");
-    long n = input.nextLong();
-    long answer = sumDigits(n);
-  
-    //Prints the answer.
-    System.out.println("The sum of the digits in your integer is " + answer);
-  }
-  
-  //Recieves number and runs it through equation.
-  public static int sumDigits(long n) {
-    int sum = 0;
-    int m = (int) n;
-  
-    //Loops until all the digits are read.
-    while (m > 0){
-      int digit = m % 10;
-      m = m / 10;
-      sum = sum + digit;
+public class Problem8_13 {
+	public static void main(String[] args) {
+    
+    //Creates a scanner.
+		Scanner input = new Scanner(System.in);
+
+	  //Prompts a 2d array from the user.
+    System.out.println("Enter the number of rows and columns of the array: ");
+    int row = input.nextInt();
+    int column = input.nextInt();
+    
+    //Sizes array.
+    double[][] array = new double[row][column];
+
+    //Prompts array data from user.
+    System.out.println("Enter the array:");
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 0; j < array[i].length; j++) {
+        array[i][j] = input.nextDouble();
+      }
     }
 
-    return sum;
-  } 
+    //Gets the location of the largest array.
+    int[] location = locateLargest(array);
+
+    //Prints the result.
+    System.out.println("The location of the largest element is at (" +
+      location[0] + ", " + location[1] + ")");
+  }
+
+  //Find the largest element in the array.
+  public static int[] locateLargest(double[][] a) {
+    int[] o = new int[2];
+    o[0] = 0;
+    o[1] = 0;
+    double max = 0;
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[i].length; j++) {
+        if (a[i][j] > max) {
+          o[0] = i;
+          o[1] = j;
+          max = a[i][j];
+        }
+      }
+    }
+    return o;
+  }
 }
